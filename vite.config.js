655b14a2f276af5f7a55b984e46f9e98031daf1b
Hao <trantuanhao308@gmail.com>
2025-04-19 17:45:29 +0700

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? '/Public_ReactWeb3/' : '/', // ✅ CHẮC CHẮN có dấu "/" cuối
-}))
